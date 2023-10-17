d23dc89fab9a313d625d5d9d5ce4a4837b0828f6
Fabio J. Walter <fabiojwalter@gmail.com>
2023-10-16 21:51:30 -0300

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,2 @@
-const String urlQuotations = "https://api.hgbrasil.com/finance?key=2e7545a7";
+//const String urlQuotations = "https://api.hgbrasil.com/finance?key=2e7545a7";
+const String urlQuotations = "https://api.hgbrasil.com/finance?key=53ca4b10"
